6a0acaaf779676cb08158103fc86bf3ee720f914
MohamedAliAyadi <ayadi.mohamedali@esprit.tn>
2022-12-03 15:23:30 +0100

    feat(*): components home, cart, shopping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ShowShoppingComponent } from './show-shopping/show-shopping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddProductComponent,
+    ShowShoppingComponent
   ],
   imports: [
     BrowserModule,
